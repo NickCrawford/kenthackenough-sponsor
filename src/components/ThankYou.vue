2b708624806f054226a1687b82342657e94da4cc
NickCrawford <ncrawfo7@kent.edu>
2018-02-02 01:54:25 -0500

    Fixes paypal image resizing on mobile

--- src/components/ThankYou.vue
+++ src/components/ThankYou.vue
@@ -7,9 +7,9 @@
     <span>
       <p class="big" v-if="$root.donationAmount > 0">Pay now with:</p>
       <div class="flex-row">
-        <!--<div class="paycard" tabindex="6" v-if="$root.donationAmount > 0">
-          <h3>Pay now with <h2>Paypal</h2></h3>-->
-          <form v-if="$root.donationAmount > 0" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paycard">
+        <div class="paycard" tabindex="6" v-if="$root.donationAmount > 0">
+          <h3>Pay now with <h2>Paypal</h2></h3>
+          <form v-if="$root.donationAmount > 0" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
             <input type="hidden" class="paypal" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="FHFGGFNLMM8YY">
             <input type="image" 
@@ -28,7 +28,7 @@
               width="1" 
               height="1">
           </form>
-        <!--</div>-->
+        </div>
         <div class="paycard" tabindex="7">
           <h3>View your <h2>invoice</h2></h3>
           <img src="@/assets/invoice.png" />
@@ -57,23 +57,27 @@ export default {
   }
 
   #paypalImg {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
+    padding-top: 4em;
+    object-fit: contain;
   }
 
-.paypal {
-  cursor: pointer;
-  height: 100%;
-  padding: 1em 2em;
-  width: 48%;
-  /*transform: scale(6);*/
-
-}
+  .paypal {
+    cursor: pointer;
+    height: 100%;
+    padding: 1em 2em;
+    width: 48%;
+  }
 
  .flex-row {
     display: flex;
     flex-wrap: nowrap;
     justify-content: space-between;
-    align-items: baseline;
+    align-items: stretch;
 
     @media screen and (max-width: 768px) {
       flex-wrap: wrap;
@@ -81,12 +85,14 @@ export default {
   }
 
 .paycard {
+    position: relative;
     background-color: #FFF;
-    /*padding: 1em 2em;*/
+    padding: 1em 2em;
     box-shadow: 0 3px 5px rgba(0,0,0,0.3);
     border-radius: 4px;
     width: 45%;
     margin: 1em 0;
+    text-align: center;
 
     cursor: pointer;
 
@@ -96,10 +102,19 @@ export default {
 
     img {
       width: 100%;
+      height: 100%;
+      object-fit: contain;
+      margin: 0 auto;
     }
 
     @media screen and (max-width: 768px) {
       width: 100%;
+      height: 13em;
+
+      img {
+        height: 70%;
+        width: auto;
+      }
     }
   }
 </style>
